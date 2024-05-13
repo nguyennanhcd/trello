22b0a9e8c4f6363ce526202c4d74f388c936c2d1
nguyenanhcd <anh487303@gmail.com>
2024-05-13 15:29:28 +0700

    custom them

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
 import Button from '@mui/material/Button'
+import { useState } from 'react'
 import { Typography } from '@mui/material'
-import {
-  useColorScheme
-} from '@mui/material/styles'
+import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  };
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <LightModeIcon fontSize='small'/> Light
+        </MenuItem>
+        <MenuItem value="dark">
+          <DarkModeIcon fontSize='small'/> Dark
+        </MenuItem>
+        <MenuItem value="system">
+          <SettingsBrightnessIcon fontSize='small'/>System
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 function App() {
   return (
     <>
-      <ModeToggle/>
+      <ModeSelect/>
+      <hr />
       <Typography variant="body2" color="text.secondary">test</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
