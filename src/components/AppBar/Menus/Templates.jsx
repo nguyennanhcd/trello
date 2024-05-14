af35f7d0539d930b812b1ebee2e1306488d02ce9
nguyenanhcd <anh487303@gmail.com>
2024-05-14 16:21:44 +0700

    rel_abs_import

---
+++ src/components/AppBar/Menus/Templates.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Check from '@mui/icons-material/Check'
+import Divider from '@mui/material/Divider'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+
+function Templates() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Button
+        id="basic-button-recent"
+        aria-controls={open ? 'basic-menu-recent' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={ <ArrowDropDownIcon/> }
+      >
+        Templates
+      </Button>
+      <Menu
+        id="basic-menu-recent"
+        sx={{ transform: 'translateY(4.5%)' }}
+        aria-labelledby="basic-button-recent"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+      >
+        <MenuItem>
+          <ListItemText inset>Single</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>1.15</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>Double</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Check />
+          </ListItemIcon>
+          Custom: 1.2
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemText>Add space before paragraph</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText>Add space after paragraph</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemText>Custom spacing...</ListItemText>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Templates
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,17 +1,65 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import { SvgIcon, Typography } from '@mui/material'
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import Tooltip from '@mui/material/Tooltip'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profiles from './Menus/Profiles'
 
 function AppBar() {
   return (
     <div>
-      <Box sx={{
-        backgroundColor: 'primary.light',
+      <Box px={2} sx={{
         width: '100%',
         height: (theme) => theme.trello.navBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
       }}>
-        <ModeSelect/>
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 2 }}>
+          
+          <AppsIcon sx={{ color:'primary.main' }} fontSize='small'/>
+
+          <Box sx={{ display: 'flex', alignItems:'center', gap: 0.5 }}>
+            <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color:'primary.main' }} fontSize='small'/>
+            <Typography variant='span' sx={{ fontSize:'1.2rem', color:'primary.main', fontWeight:'bold' }}>Dloo</Typography>
+          </Box>
+
+          <Workspaces/>
+
+          <Recent/>
+
+          <Starred/>
+
+          <Templates/>
+          <Button variant="outlined">Create</Button>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems:'center', gap: 2 }}>
+          <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+
+          <ModeSelect/>
+
+          <Tooltip title="Notification">
+            <Badge color="secondary" variant="dot" sx={{ cursor:'pointer' }}>
+              <NotificationsNoneIcon />
+            </Badge>
+          </Tooltip>
+
+          <Tooltip title="Help" sx={{ cursor:'pointer' }}>
+            <HelpOutlineIcon/>
+          </Tooltip>
+
+          <Profiles/>
+        </Box>
       </Box>
     </div>
   )
