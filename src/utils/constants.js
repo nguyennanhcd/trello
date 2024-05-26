ddd15621b33881388a4180ec3cf9660c913c983f
nguyenanhcd <anh487303@gmail.com>
2024-05-26 14:26:35 +0700

    fix dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,12 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev')
+{
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production')
+{
+  apiRoot = 'https://dngdung.vercel.app/'
+}
+
+export const API_ROOT = apiRoot
